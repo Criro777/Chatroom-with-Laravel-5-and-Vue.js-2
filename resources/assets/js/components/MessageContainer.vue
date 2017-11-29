f88327cfd605702749ba781080a7f73594199c07
Unknown <russiancardcollector@mail.ru>
2017-11-29 23:39:04 +0300

    Изменения в компоненте

--- resources/assets/js/components/MessageContainer.vue
+++ resources/assets/js/components/MessageContainer.vue
@@ -1,6 +1,7 @@
 <template lang="html">
     <div class="chat-log">
         <message v-for="message in messages" :message="message"></message>
+        <div class="empty" v-show="messages.length === 0">Сообщений пока нет!</div>
     </div>
 </template>
 
@@ -13,5 +14,9 @@
 <style lang="css">
     .chat-log .chat-message:nth-child(even) {
         background-color: #ccc;
+    }
+    .empty {
+        padding: 1rem;
+        text-align: center;
     }
 </style>
